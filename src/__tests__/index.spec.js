f3db35d7a08054c97599b02f4f5d7ff17fcf5160
Jordan Sinko <jordan.sinko@amway.com>
2018-10-08 10:24:09 -0400

    changing file structure

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,10 +1,10 @@
 const test = require('tape');
-const destructure-promise = require('..');
+const destructure = require('..');
 
 test('destructure-promise handles resolved promises', assert => {
   assert.plan(2);
   const promiseFn = arg => new Promise(res => setTimeout(res, 0, arg));
-  const fn = destructure-promise(promiseFn);
+  const fn = destructure(promiseFn);
   fn('test').then(({ error, response }) => {
     assert.equals(error, null);
     assert.equals(response, 'test');
@@ -15,7 +15,7 @@ test('destructure-promise handles rejected promises', assert => {
   assert.plan(2);
   const promiseFn = () =>
     new Promise((_, rej) => setTimeout(rej, 0, new Error('This is an error')));
-  const fn = destructure-promise(promiseFn);
+  const fn = destructure(promiseFn);
   fn('test').then(({ error, response }) => {
     assert.equals(error.message, 'This is an error');
     assert.equals(response, null);
